daa059c44ae9805bcfed1aaa5c01991ff49cabd1
Hyon Woo Kang <87benzenes@gmail.com>
2026-01-22 21:40:32 +0900

    remove service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [],
-  providers: [AppService],
 })
 export class AppModule {}
